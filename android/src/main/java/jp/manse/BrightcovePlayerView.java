a0803bb827995f7a45a254a51482824bbdbe152d
Eden <edenlc@gmail.com>
2021-11-04 10:25:31 +1300

    migrate to androidx

--- android/src/main/java/jp/manse/BrightcovePlayerView.java
+++ android/src/main/java/jp/manse/BrightcovePlayerView.java
@@ -1,7 +1,7 @@
 package jp.manse;
 
 import android.graphics.Color;
-import android.support.v4.view.ViewCompat;
+import androidx.core.view.ViewCompat;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.widget.RelativeLayout;
